b9b7f316d97d49cfa5001c6df0c09f8fea1aedd3
Gary Connelly <G00336837@gmit.ie>
2019-03-25 18:06:32 +0000

    Started with padding.

---
+++ padfile.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdint.h>
+
+union msgblock{
+  uint8_t e[64];
+  uint32_t t[16];
+  uint64_t s[8];
+
+};
+
+int main(int argc, char *argv[]){
+
+  union msgblock M;
+
+  uint64_t nobits = 0;
+
+  uint64_t nobytes;
+
+  FILE* file;
+  file = fopen(argv[1], "r");
+
+  while(!feof(file)){
+    nobytes = fread(M.e, 1, 64, file);
+    printf("Read %2llu bytes\n", nobytes);
+    nobits = nobits + (nobytes * 8);
+    
+    if(nobytes < 56){
+      printf("I've found a block with less than 55 bytes!\n");
+      M.e[nobytes] = 0x80;
+      while(nobytes < 56){
+        nobytes = nobytes + 1;
+        M.e[nobytes] = 0x00;
+      }
+      M.s[7] = nobits;
+    }
+    
+
+  }
+
+  fclose(file);
+
+  for (int i = 0; i < 64; i++){
+    printf("%x ", M.e[i]);
+  }
+  printf("\n");
+
+
+  return 0;
+
+}
--- sha256.c
+++ sha256.c
@@ -23,12 +23,36 @@ int main (int argc, char *argv[]){
 
   sha256();
 
+
+  
   return 0;
 
 }
 
 void sha256(){
 
+
+  uint32_t K[] = {
+    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
+    0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
+    0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
+    0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
+    0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
+    0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
+    0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
+    0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+    0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+
+
+};
+ 
   uint32_t W[64];
 
   uint32_t a,b,c,d,e,f,g,h;
@@ -47,16 +71,20 @@ void sha256(){
     0x1f83d9ab,
     0x5be0cd19
   };
+  
+  uint32_t M[16]  = {0,0,0,0,0,0,0,0};
 
   // For looping.
-  int t;  
+  int t,i;  
+  
+  for( i = 0; i < 1; i ++){
   
   // From page 22, W[t] = M[t] for 0 <= t <= 15.
   for(t = 0; t < 16; t++)
     W[t] = M[t];
 
   for(t = 16; t < 64; t++)
-    W[t] = sig_1(W[t-2]) + W[t-7] + sig_0(W[t-15]) + W[t-16];
+    W[t] = sig1(W[t-2]) + W[t-7] + sig0(W[t-15]) + W[t-16];
 
   
   // Initialize a,b,c, ... ,h as per step 2, Page 22.
@@ -89,7 +117,7 @@ void sha256(){
   H[6] = g + H[6];
   H[7] = h + H[7];
 
-  
+  }
 }
 
 uint32_t sig0(uint32_t x){
